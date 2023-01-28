916ab24a77492fc1737e258e967476a67056e1b5
TheNewBadger <83123367+andywoodruff6@users.noreply.github.com>
2023-01-28 17:54:21 -0600

    Calculate per share complete
    Works but doesn't handle nil at all.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -2,14 +2,18 @@
 //  ViewController.swift
 //  Tipsy
 //
-//  Created by Angela Yu on 09/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Andy W. on 01/28/2023.
+//  Copyright © 2023 The New Badger. All rights reserved.
 //
+//  This works but is not error safe at all.
 
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
+    var numberOfPartyMembers: Float = 0
+    var tipPct: Float?
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
@@ -17,10 +21,33 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        var title: String = sender.currentTitle!
+        var trimmedTitle = title.dropLast(1)
+        var trimmedTitleToNumber: Float = Float(trimmedTitle) ?? 0.0
+        var convertToPct = (trimmedTitleToNumber / 100)
+        tipPct = convertToPct
+//        print(convertToPct)
+        billTextField.endEditing(true)
     }
+    
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
+        numberOfPartyMembers = Float(sender.value)
+        billTextField.endEditing(true)
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(numberOfPartyMembers)
+        let billValue = Float(billTextField.text ?? "0.0")
+        let totalBill = (billValue! * tipPct!) + billValue!
+        
+        let billShare = totalBill / numberOfPartyMembers
+        print(billShare)
     }
 }
 
